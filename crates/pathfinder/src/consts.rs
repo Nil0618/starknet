0b0af308595b946c9cd6734e3d6a485a204b4469
Nil0618 <107695446+Nil0618@users.noreply.github.com>
2022-06-17 20:43:41 +0700

    Delete crates directory

--- crates/pathfinder/src/consts.rs
+++
@@ -1,7 +0,0 @@
-//! Repeated constants used around pathfinder
-
-/// User agent used in http clients
-pub const USER_AGENT: &str = concat!(
-    "starknet-pathfinder/",
-    env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT")
-);
--- crates/stark_hash/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![deny(rust_2018_idioms)]
-
-mod hash;
-mod serde;
-
-pub use hash::{stark_hash, HexParseError, OverflowError, StarkHash};
